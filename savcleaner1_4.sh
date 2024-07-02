e0ad2e197164139b3223e3f0a223f22a9f8f337c
TB <149841209+u2pitchjami@users.noreply.github.com>
2024-07-02 14:52:54 +0200

    cleanup and add explications

--- savcleaner1_4.sh
+++ savcleaner1_4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ############################################################################## 
 #                                                                            #
-#	SHELL: !/bin/bash       version 1.4                                      #
+#	SHELL: !/bin/bash       version 1.5                                      #
 #									                                         #
 #	NOM: BEUGNET							                                 #
 #									                                         #
@@ -12,21 +12,23 @@
 #	BUT: Script pour nettoyer les sauvegarde AB                 		     #
 #									                                         #
 ############################################################################## 
+
+
+######CONFIGURATION###########################################################
 DATE=$(date "+%y%m%d_%H%M")
 LOG=savcleaner.txt
-DOSSIERLOGS="C:/Users/thierry/Documents/logs/" #a modifier si besoin
-DIRLOGAB="C:\ProgramData\AomeiBR\brlog.xml"
-#DIRLOGAB="brlog.xml"
+DOSSIERLOGS="C:/SCRIPTS LOGS DIRECTORY/" #a modifier si besoin
+DIRLOGAB="C:\ProgramData\AomeiBR\brlog.xml" #dossier log Aomei
 HOST=$(hostname | tr a-z A-Z) # Récupère le hostname et le transforme en majuscules
-#HOST=$(echo $HOST | tr a-z A-Z)
 LECTEUR=$"H:" # Mettre la lettre de votre lecteur de sauvegarde sur votre ordinateur
-#LECTEUR=$"./test" 
 NBSAV=1 #Nb de sauvegarde complètes à conserver
+######CONFIGURATION###########################################################
 
-if [ ! -d $DOSSIERLOGS ];then
-echo "Création du dosser LOGS !";
-mkdir $DOSSIERLOGS
-fi
+    if [ ! -d $DOSSIERLOGS ]
+    then
+    echo "Création du dosser LOGS !"
+    mkdir $DOSSIERLOGS
+    fi
 
 touch "$DOSSIERLOGS"$DATE"$LOG"
 #traitement du fichier log, retire les lignes et balises inutiles
@@ -39,37 +41,37 @@ tac INTER2 | sed '/complète/!d;/'Succès/!d > INTER3 #ne conserve que les sav c
 
 #va chercher les dossiers de sauvegardes pour les traiter 1 par 1
 NBL=$(ls -lh $LECTEUR/ | grep ^d | grep "$HOST"$ | wc -l) 
-for (( c=1; c<=$NBL; c++ ))
-do
-BACKUP=$(ls -lh $LECTEUR/ | grep ^d | grep "$HOST"$ | tr -s " "| cut -d " " -f9 | head -$c | tail +$c)
-echo "Dossier de sauvegarde : ${BACKUP}" | tee -a "$DOSSIERLOGS"$DATE"$LOG"
-#va chercher les sauvegardes complètes réussis en fonction du nb à conserver (le log est par défaut dans l'ordre chono)
-NBL2=$(cat INTER3 | grep "$BACKUP" | head -$NBSAV | tail +$NBSAV | wc -l)
-for (( d=1 ; d<=$NBL2 ; d++  ))
-do
-LINE=$(cat INTER3 | grep "$BACKUP")
-#transforme la date du format timestamp à un format exploitable
-TIME0=`echo $LINE | cut -d ";" -f1`
-TIME=`date -d "@$TIME0" +%Y%m%d`
-SAUVEGARDE=`echo $LINE | cut -d ";" -f2`
-TYPESAV=`echo $LINE | cut -d ";" -f3`
-OK0PASOK1=`echo $LINE | cut -d ";" -f4`
-#echo "${TIME};${SAUVEGARDE};${TYPESAV};${OK0PASOK1}" >> brlog.csv
-echo "La dernière sauvegarde complète date de : ${TIME}" | tee -a "$DOSSIERLOGS"$DATE"$LOG"
+    for (( c=1; c<=$NBL; c++ ))
+    do
+    BACKUP=$(ls -lh $LECTEUR/ | grep ^d | grep "$HOST"$ | tr -s " "| cut -d " " -f9 | head -$c | tail +$c)
+    echo "Dossier de sauvegarde : ${BACKUP}" | tee -a "$DOSSIERLOGS"$DATE"$LOG"
+    #va chercher les sauvegardes complètes réussis en fonction du nb à conserver (le log est par défaut dans l'ordre chono)
+    NBL2=$(cat INTER3 | grep "$BACKUP" | head -$NBSAV | tail +$NBSAV | wc -l)
+        for (( d=1 ; d<=$NBL2 ; d++  ))
+        do
+        LINE=$(cat INTER3 | grep "$BACKUP")
+        #transforme la date du format timestamp à un format exploitable
+        TIME0=`echo $LINE | cut -d ";" -f1`
+        TIME=`date -d "@$TIME0" +%Y%m%d`
+        SAUVEGARDE=`echo $LINE | cut -d ";" -f2`
+        TYPESAV=`echo $LINE | cut -d ";" -f3`
+        OK0PASOK1=`echo $LINE | cut -d ";" -f4`
 
-#cherche les fichiers plus anciens que la date des logs (le -newer est pour identifier les fichiers plus récents mas le ! permet de l'inverser)
-NBFILES=$(find "$LECTEUR"/"$BACKUP"/ -type f ! -newerBt $TIME | wc -l)
-if [ $NBFILES -gt 0 ]
-then
-echo "Il y a $NBFILES fichiers à supprimer" | tee $DATE$LOG
-find "$LECTEUR"/"$BACKUP"/ -type f ! -newerBt $TIME | tee -a "$DOSSIERLOGS"$DATE"$LOG"
-find "$LECTEUR"/"$BACKUP"/ -type f ! -newerBt $TIME -exec rm {} \;
-echo "Suppression terminée" | tee -a "$DOSSIERLOGS"$DATE"$LOG"
-else
-echo "aucun fichier à supprimer" | tee -a "$DOSSIERLOGS"$DATE"$LOG"
-fi
-done
-done
+        echo "La dernière sauvegarde complète date de : ${TIME}" | tee -a "$DOSSIERLOGS"$DATE"$LOG"
+
+        #cherche les fichiers plus anciens que la date des logs (le -newer est pour identifier les fichiers plus récents mas le ! permet de l'inverser)
+        NBFILES=$(find "$LECTEUR"/"$BACKUP"/ -type f ! -newerBt $TIME | wc -l)
+            if [ $NBFILES -gt 0 ]
+            then
+            echo "Il y a $NBFILES fichiers à supprimer" | tee $DATE$LOG
+            find "$LECTEUR"/"$BACKUP"/ -type f ! -newerBt $TIME | tee -a "$DOSSIERLOGS"$DATE"$LOG"
+            find "$LECTEUR"/"$BACKUP"/ -type f ! -newerBt $TIME -exec rm {} \;
+            echo "Suppression terminée" | tee -a "$DOSSIERLOGS"$DATE"$LOG"
+            else
+            echo "aucun fichier à supprimer" | tee -a "$DOSSIERLOGS"$DATE"$LOG"
+            fi
+        done
+    done
 #suppression des fichiers temporaires
 rm INTER*
 sleep 60
